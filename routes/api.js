ea4a838833eb3adfe02de4a5b95f1218d0071d3d
nate.dean <nate.dean@pathar.net>
2016-01-31 17:03:02 -0600

    Much more work done on jwt token auth setup

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var login = require('./api/login');
 
 router.get('/music-theory/question', require('./api/music-theory/question'));
 
-router.post('/login', require('./api/login'));
+router.post('/login', login.authenticateCredentials, login.successCallback);
 
 module.exports = router;
--- routes/api/login.js
+++ routes/api/login.js
@@ -1,3 +1,30 @@
-module.exports = function(req, res, next){
-    res.send('you sent a post to login!');
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'asdf'; //TODO: Make ENV variable
+
+// fake user from database
+const user = {
+    username: 'nate',
+    password: 'test'
 };
+
+const successCallback = (req, res, next) => {
+    var token = jwt.sign({ username: 'test' }, jwtSecret);
+
+    res.redirect(`/?token=${token}`);
+};
+
+const authenticateCredentials = (req, res, next) => {
+    var body = req.body;
+
+    if(!body.username || !body.password){
+        res.status(400).end('Must provide username or password');
+    }
+    // TODO: hook to database
+    if(body.username !== user.username || body.password !== user.password){
+        res.status(401).end('Username of password incorrect');
+    }
+
+    next();
+};
+
+module.exports = { authenticateCredentials, successCallback };
