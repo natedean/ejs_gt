5363e28074e88ecd82a8855cf0f555c50cefda1c
Nate Dean <nate.dean@pathar.net>
2016-07-03 05:46:35 -0500

    Removed mongo hookup for testing. Differentiated between start and dev npmm tasks

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // db hookup
-mongoose.connect('mongodb://localhost/gt');
+// mongoose.connect('mongodb://localhost/gt');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
