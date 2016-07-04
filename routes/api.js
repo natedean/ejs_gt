1a913428a8b2e7445ffbf673ee22f4ba04f346fb
Nate Dean <nate.dean@pathar.net>
2016-07-04 13:12:29 -0500

    Fixed 'signup' vs 'signUp' bug. Removed html links that are irrelevant for the moment

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var signUp = require('./api/signUp');
+var signup = require('./api/signup');
 var login = require('./api/login');
 
 router.get('/music-theory/question', require('./api/music-theory/question'));
 
-router.post('/signUp', signUp, login.successCallback);
+router.post('/signup', signup, login.successCallback);
 router.post('/login', login.authenticateCredentials, login.successCallback);
 
 module.exports = router;
