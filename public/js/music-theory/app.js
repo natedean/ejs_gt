6491e9dcad577b3621fc6c4bcf17e5e8ceb224c6
Nate Dean <nate.dean@pathar.net>
2016-07-09 12:11:01 -0500

    A working, kind of complex, mostly ugly rxjs/fetch/dom-manipulation example is working

--- public/js/music-theory/app.js
+++ public/js/music-theory/app.js
@@ -4,29 +4,70 @@ var initialState = {
 };
 
 var increaseButton = document.querySelector('#increase');
-var increase = Rx.Observable.fromEvent(increaseButton, 'click')
-  // Again we map to a function the will increase the count
-  .map(() => state => state.set('count', state.get('count') + 1));
-
 var decreaseButton = document.querySelector('#decrease');
-var decrease = Rx.Observable.fromEvent(decreaseButton, 'click')
-  // We also map to a function that will decrease the count
-  .map(() => state => state.set('count', state.get('count') - 1));
-
 var inputElement = document.querySelector('#input');
-var input = Rx.Observable.fromEvent(inputElement, 'keyup')
-  // Let us also map the keypress events to produce an inputValue state
-  .map(event => state => state.set('inputValue', event.target.value));
+var eId = document.querySelector('.personDisplay-id');
+var eName = document.querySelector('.personDisplay-name');
+var eEmail = document.querySelector('.personDisplay-email');
+var eLoading = document.querySelector('.loadingIndicator');
+
+var increase = Rx.Observable.fromEvent(increaseButton, 'click')
+    .map(e => e.target.value);
+  // .map(() => state => state.set('count', state.get('count') + 1));
+
+
+var decrease = Rx.Observable.fromEvent(decreaseButton, 'click')
+    .map(e => e.target.value);
+  // .map(() => state => state.set('count', state.get('count') - 1));
+
+
+var state$ = Rx.Observable.merge(increase, decrease)
+    .startWith(1)
+    .map(x => parseInt(x, 10))
+    .scan((acc, x) => {
+      if (acc <= 1 && x < 1) return acc;
+
+      return acc + x;
+    }, 0)
+    .filter(x => x > 0)
+    .do(val => setLoadingState())
+    .debounceTime(300)
+    .switchMap((val) => fetch(`https://jsonplaceholder.typicode.com/users/${val}`))
+    .flatMap(res => res.json())
+    .do(val => eLoading.textContent = '');
+
+state$.subscribe(val => {
+  eId.textContent = val.id;
+  eName.textContent = val.name;
+  eEmail.textContent = val.email;
+
+
+  if(val.id <= 1){
+    decreaseButton.style.display = 'none';
+  } else {
+    decreaseButton.style.display = 'inline-block';
+  }
+
+});
+
+function setLoadingState() {
+  eName.textContent = 'Loading...';
+  eEmail.textContent = '';
+}
+
+
+
 
 // We merge the three state change producing observables
-var state = Rx.Observable.merge(
-  increase,
-  decrease,
-  input
-).scan((state, changeFn) => changeFn(state), Immutable.fromJS(initialState));
-
-// We subscribe to state changes and update the DOM
-state.subscribe((state) => {
-  document.querySelector('#count').innerHTML = state.get('count');
-  document.querySelector('#hello').innerHTML = 'Hello ' + state.get('inputValue');
-});
+// var state$ = Rx.Observable.merge(
+//   increase,
+//   decrease,
+//   input
+// ).scan((state, changeFn) => changeFn(state), Immutable.fromJS(initialState));
+//
+// // We subscribe to state changes and update the DOM
+// state$.subscribe((state) => {
+//   document.querySelector('#count').innerHTML = state.get('count');
+//   document.querySelector('#hello').innerHTML = 'Hello ' + state.get('inputValue');
+//   console.log(state.get('inputValue'));
+// });
