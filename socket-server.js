1feeaecdc8f12dc512dfcb6444f57c643cb6860e
Nate Dean <nate.dean@pathar.net>
2016-08-04 23:22:44 -0500

    Just did a batcrap load of work to refactor to immutable. I think it will be worth it... after I get done crying. Also, I'm tired.

--- socket-server.js
+++ socket-server.js
@@ -2,61 +2,63 @@
 
 const io = require('./app').io;
 const uuid = require('node-uuid');
+const Immutable = require('immutable');
 
-const createNewRoom = (playerId) => ({
-  id: uuid.v4(),
-  players: []
+let RoomsMap = Immutable.Map();
+
+const createNewRoom = (playerId) => {
+  let map = new Immutable.Map();
+
+  map.set(playerId, createPlayer());
+
+  return map;
+};
+
+const createPlayer = () => ({
+  correctAnswers: 0,
+  incorrectAnswers: 0
 });
 
-const isVacancies = (rooms) => rooms.filter(x => x.players.length < 2).length > 0;
+const hasVacancy = (RoomsMap) => RoomsMap.filter(x => x.size < 2).size > 0;
 
-const addRoomIfNecessary = (rooms, playerId) => {
-  if (rooms.length && isVacancies(rooms)) { return rooms; }
+const addRoomIfNecessary = (RoomsMap, playerId) => {
+  if (RoomsMap.size && hasVacancy(RoomsMap)) { return RoomsMap; }
 
-  return rooms.concat([createNewRoom(playerId)]);
+  return RoomsMap.set(uuid.v4(), createNewRoom(playerId));
 };
 
-const addPlayer = (rooms, roomId, playerId) => {
-  return rooms.map((x) => {
-    if (x.id !== roomId) { return x; }
-
-    return Object.assign({}, x, { players: x.players.concat([playerId]) });
-  });
+const findAvailableRoomId = (RoomsMap) => {
+  return RoomsMap.findKey(x => x.size < 2);
 };
-
-const findAvailableRoomId = (rooms) => rooms.filter(x => x.players.length < 2)[0].id;
-
-let rooms = [];
 
 io.on('connection', function (socket) {
   const playerId = socket.client.id; // this really needs to pass the players mongo uid if available
 
   // if there are no empty rooms, add a room.
-  rooms = addRoomIfNecessary(rooms);
+  RoomsMap = addRoomIfNecessary(RoomsMap, playerId);
 
   // find the first available room id
-  let roomId = findAvailableRoomId(rooms);
+  let roomId = findAvailableRoomId(RoomsMap);
 
   // add player to the first available room
-  rooms = addPlayer(rooms, roomId, playerId);
-
-  const myRoom = rooms.find(x => x.id === roomId);
+  RoomsMap = RoomsMap.setIn([roomId, playerId], createPlayer());
 
   // socket, join room
   socket.join(roomId);
 
+  socket.emit('joined', roomId);
+
   socket.on('answerEvent', (data) => {
-
-
+    // update game object!
 
     socket.emit('answerEvent', myRoom); // the client will optimistically update, but then we need to sync up just in case
     socket.to(roomId).emit('answerEvent', myRoom); // notify the room!
   });
 
   // start game, if able
-  if (myRoom.players.length === 2) {
-    socket.emit('startGame', myRoom); // tell me to startGame
-    socket.to(roomId).emit('startGame', myRoom); // tell others in room to startGame
+  if (RoomsMap.get(roomId).size === 2) {
+    socket.emit('startGame', RoomsMap.get(roomId).toJS()); // tell me to startGame
+    socket.to(roomId).emit('startGame', RoomsMap.get(roomId).toJS()); // tell others in room to startGame
   }
 
   socket.on('disconnect', () => console.log(`disconnected from ${roomId}`));
