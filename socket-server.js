97c96c6031cec1afc2830107f4050cc2c4a7d202
Nate Dean <nate.dean@pathar.net>
2016-08-06 16:18:04 -0500

    A lot of good stuff.

--- socket-server.js
+++ socket-server.js
@@ -6,12 +6,11 @@ const Immutable = require('immutable');
 
 let RoomsMap = Immutable.Map();
 
-const createNewRoom = (playerId) => {
-  let map = new Immutable.Map();
-
-  map.set(playerId, createPlayer());
-
-  return map;
+const createNewRoom = () => {
+  return Immutable.Map({
+    gameIsInProgress: false,
+    players: Immutable.Map()
+  });
 };
 
 const createPlayer = () => ({
@@ -19,29 +18,29 @@ const createPlayer = () => ({
   incorrectAnswers: 0
 });
 
-const hasVacancy = (RoomsMap) => RoomsMap.filter(x => x.size < 2).size > 0;
+const hasVacancy = (RoomsMap) => RoomsMap.filter(x => x.get('players').size < 2).size > 0;
 
 const addRoomIfNecessary = (RoomsMap, playerId) => {
   if (RoomsMap.size && hasVacancy(RoomsMap)) { return RoomsMap; }
 
-  return RoomsMap.set(uuid.v4(), createNewRoom(playerId));
+  return RoomsMap.set(uuid.v4(), createNewRoom());
 };
 
 const findAvailableRoomId = (RoomsMap) => {
-  return RoomsMap.findKey(x => x.size < 2);
+  return RoomsMap.findKey(x => x.get('players').size < 2);
 };
 
 io.on('connection', function (socket) {
   const playerId = socket.client.id; // this really needs to pass the players mongo uid if available
 
   // if there are no empty rooms, add a room.
-  RoomsMap = addRoomIfNecessary(RoomsMap, playerId);
+  RoomsMap = addRoomIfNecessary(RoomsMap);
 
   // find the first available room id
   let roomId = findAvailableRoomId(RoomsMap);
 
   // add player to the first available room
-  RoomsMap = RoomsMap.setIn([roomId, playerId], createPlayer());
+  RoomsMap = RoomsMap.setIn([roomId, 'players', playerId], createPlayer());
 
   // socket, join room
   socket.join(roomId);
@@ -55,11 +54,18 @@ io.on('connection', function (socket) {
     socket.to(roomId).emit('answerEvent', myRoom); // notify the room!
   });
 
+  console.log('somebody joined! RoomsMap.size: ', RoomsMap.size);
+
   // start game, if able
-  if (RoomsMap.get(roomId).size === 2) {
+  if (RoomsMap.getIn([roomId, 'players']).size === 2) {
+    RoomsMap = RoomsMap.setIn([roomId, 'gameIsInProgress'], true);
     socket.emit('startGame', RoomsMap.get(roomId).toJS()); // tell me to startGame
     socket.to(roomId).emit('startGame', RoomsMap.get(roomId).toJS()); // tell others in room to startGame
   }
 
-  socket.on('disconnect', () => console.log(`disconnected from ${roomId}`));
+  socket.on('disconnect', () => {
+    RoomsMap = RoomsMap.removeIn([roomId, 'players', playerId]);
+    RoomsMap = RoomsMap.setIn([roomId, 'gameIsInProgress'], false);
+    socket.to(roomId).emit('endGame', RoomsMap.get(roomId).toJS()); // tell others in room to startGame
+  });
 });
